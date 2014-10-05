3ec8bba99b2dd55e4839846681a43e1222a97cf1
cohen990 <dan.cohen@mopowered.co.uk>
2014-10-05 15:50:22 +0100

    Removed boilerplate code

--- ThisIsJokes/ThisIsJokes.Tests/Controllers/HomeControllerTest.cs
+++ ThisIsJokes/ThisIsJokes.Tests/Controllers/HomeControllerTest.cs
@@ -1,54 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ThisIsJokes;
-using ThisIsJokes.Controllers;
-
-namespace ThisIsJokes.Tests.Controllers
+﻿namespace ThisIsJokes.Tests.Controllers
 {
-    [TestClass]
+    using System.Web.Mvc;
+    using ThisIsJokes.Controllers;
+    using Xunit;
+
     public class HomeControllerTest
     {
-        [TestMethod]
+        [Fact]
         public void Index()
         {
             // Arrange
-            HomeController controller = new HomeController();
+            var controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Index() as ViewResult;
+            var result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
+            Assert.NotNull(result);
         }
     }
 }
--- ThisIsJokes/ThisIsJokes/Global.asax.cs
+++ ThisIsJokes/ThisIsJokes/Global.asax.cs
@@ -19,10 +19,7 @@ namespace ThisIsJokes
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }
